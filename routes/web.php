223b886213632e24c39da16f8c2e3ca6cf5cd9a0
Eldar Bauyrzhan <eldarbauyrzhan@Eldars-MacBook-Air.local>
2024-05-08 23:57:20 +0500

    vacancies.index page is modified

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VacancyController;
 
+Route::get('', fn()=>to_route('vacancies.index'));
+
 Route::resource('vacancies', VacancyController::class)
     ->only(['index']);
